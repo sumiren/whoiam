e64a8b5fd31efe0b4495d55150993bcc33ec07e3
sumiren <oota.software@gmail.com>
2022-08-13 02:00:01 +0900

    feat: add mobile header menu overlay

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {Dispatch, SetStateAction} from "react";
 import {
   ActionIcon,
   Burger,
@@ -7,31 +7,41 @@ import {
 } from "@mantine/core";
 import { IconMoonStars, IconSun } from "@tabler/icons";
 
-export const Header = () => {
-  const [opened, setOpened] = useState(false);
+interface HeaderProps {
+  burgerOpenedState: boolean;
+  setBurgerOpenedState:  Dispatch<SetStateAction<boolean>>;
+}
+
+export const Header = ({ setBurgerOpenedState, burgerOpenedState }: HeaderProps) => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
   const dark = colorScheme === "dark";
-
   return (
-    // @ts-ignore
-    <OriginalHeader className="h-16 p-2 flex justify-between">
-      <div className="flex items-center">
-        <Burger opened={opened} onClick={() => setOpened((o) => !o)} />
-      </div>
-      <div className="flex text-2xl h-full items-center font-bold">
-        Sumiren Portfolio
-      </div>
-      <div className="flex items-center">
-        <ActionIcon
-          variant="outline"
-          color={dark ? "yellow.4" : "dark.4"}
-          size="lg"
-          onClick={() => toggleColorScheme()}
-        >
-          {dark ? <IconSun size={24} /> : <IconMoonStars size={24} />}
-        </ActionIcon>
-      </div>
-    </OriginalHeader>
+    <>
+      <OriginalHeader className="h-16 p-2 flex justify-between z-10">
+        <div className="flex items-center w-9"></div>
+        <div className="flex text-2xl h-full items-center font-bold">
+          Sumiren Portfolio
+        </div>
+        <div className="flex items-center">
+          <ActionIcon
+            variant="outline"
+            color={dark ? "yellow.4" : "dark.4"}
+            size="lg"
+            onClick={() => toggleColorScheme()}
+          >
+            {dark ? <IconSun size={24} /> : <IconMoonStars size={24} />}
+          </ActionIcon>
+        </div>
+      </OriginalHeader>
+      <Burger
+        opened={burgerOpenedState}
+        onClick={() => {
+          setBurgerOpenedState(!burgerOpenedState);
+        }}
+        color={burgerOpenedState ? "white" : undefined}
+        className="z-50 fixed top-4 left-4"
+      />
+    </>
   );
 };
--- frontend/layouts/layout.tsx
+++ frontend/layouts/layout.tsx
@@ -1,18 +1,57 @@
-import { AppShell, Footer } from "@mantine/core";
-import { ReactElement } from "react";
+import {AppShell, Footer, List, Overlay, Transition} from "@mantine/core";
+import {ReactElement, useEffect, useState} from "react";
 import { Header } from "../components/header";
+import {useScrollLock} from "@mantine/hooks";
+import Link from "next/link";
 
 export const Layout = ({ children }: { children: ReactElement }) => {
+  const [headerMenuOverlayOpened, setHeaderMenuOverlayOpened] = useState(false);
+  const [, setScrollLocked] = useScrollLock();
+
+  const listItemClass = "text-white text-3xl font-semibold py-3 px-5";
+
+  useEffect(()=> {
+    setScrollLocked(headerMenuOverlayOpened)
+  }, [headerMenuOverlayOpened]);
+
   return (
     <>
+      {
+        <Transition transition="fade" mounted={headerMenuOverlayOpened}>
+          {(styles) => (
+            <Overlay
+              style={{ ...styles }}
+              className="h-full w-full bg-pink-500 z-40 fixed top-0 left-0"
+              opacity="1"
+            >
+              <List className="mt-16">
+                <List.Item className={listItemClass} onClick={()=>{setHeaderMenuOverlayOpened(false)}}>
+                  <Link href="/about">
+                    About
+                  </Link>
+                </List.Item>
+                <List.Item className={listItemClass}>Blog</List.Item>
+                <List.Item className={listItemClass}>Portfolio</List.Item>
+                <List.Item className={listItemClass}>Contact</List.Item>
+              </List>
+            </Overlay>
+          )}
+        </Transition>
+      }
       <AppShell
+        className={headerMenuOverlayOpened ? "overflow-hidden" : undefined}
         classNames={{
           main: "p-0",
         }}
-        header={<Header></Header>}
+        header={
+          <Header
+            burgerOpenedState={headerMenuOverlayOpened}
+            setBurgerOpenedState={setHeaderMenuOverlayOpened}
+          ></Header>
+        }
         footer={
           // @ts-ignore
-          <Footer className="h-8 p-1">
+          <Footer className="h-8 p-1 z-30">
             <div></div>
           </Footer>
         }
