0a2b662d81a0ec84f0607370354913359cfa5cc7
sumiren <oota.software@gmail.com>
2022-08-15 00:16:11 +0900

    feat: add CI

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -5,5 +5,13 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ["text", "json-summary", "json"],
+      lines: 70,
+      branches: 70,
+      functions: 70,
+      statements: 70,
+    },
   },
 });
