19f7b794d5ca80e0514c30233d8311034cb66b81
sumiren <oota.software@gmail.com>
2022-08-15 14:59:28 +0900

    feat: add contact page

--- frontend/pages/contact.tsx
+++ frontend/pages/contact.tsx
@@ -2,40 +2,71 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import SimpleHeadlineAndTitleSection from "../components/simple-headline-and-title-section";
 import PaddingXWrapper from "../components/padding-x-wrapper";
-import {Group, TextInput, Button, Textarea} from "@mantine/core";
+import { TextInput, Button, Textarea } from "@mantine/core";
+import { useForm } from "@mantine/form";
+
+interface FormValues {
+  email: string;
+  name: string;
+  message: string;
+}
 
 const Contact: NextPage = () => {
+  const form = useForm<FormValues>({
+    initialValues: {
+      email: "",
+      name: "",
+      message: "",
+    },
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "email is invalid."),
+    },
+  });
+
+  const handleValidationPassed = (values: FormValues) => {
+    console.log("success!");
+    console.log(values);
+  };
+
   return (
     <div>
       <Head>
-        <title>Conctact</title>
+        <title>Contact</title>
         <meta name="description" content="Contact" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <PaddingXWrapper className="pt-8">
           <SimpleHeadlineAndTitleSection headline="Contact">
-            {/*<form onSubmit={form.onSubmit((values) => console.log(values))}>*/}
-            <form>
+            <form onSubmit={form.onSubmit(handleValidationPassed)}>
               <TextInput
                 required
                 label="Email"
                 placeholder="your@email.com"
+                className="mt-5"
+                {...form.getInputProps("email")}
               />
               <TextInput
                 required
-                label="Email"
-                placeholder="your@email.com"
+                label="Name"
+                placeholder="Taro Yamada"
+                className="mt-5"
+                {...form.getInputProps("name")}
               />
               <Textarea
                 required
-                label="Email"
-                placeholder="your@email.com"
+                label="Your Message"
+                placeholder="Your message"
+                autosize
+                minRows={2}
+                className="mt-5"
+                {...form.getInputProps("message")}
               />
-
-              <Group position="center" mt="md">
-                <Button type="submit">Submit</Button>
-              </Group>
+              <div className="flex justify-center mt-10">
+                <Button type="submit" size="lg">
+                  Send message
+                </Button>
+              </div>
             </form>
           </SimpleHeadlineAndTitleSection>
         </PaddingXWrapper>
