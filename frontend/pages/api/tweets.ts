ecc783acc562ab189e6785c89a9063281dabedec
sumiren <oota.software@gmail.com>
2022-09-13 07:34:02 +0900

    fix: remove unnecessary comments

--- frontend/pages/api/tweets.ts
+++ frontend/pages/api/tweets.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { fetchTweets } from "../../lib/twitter-gateway";
 
 export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  // API RouteはPOSTできる権限の鍵を隠蔽したいだけのため、バリデーションは一旦オミット
   const tweets = await fetchTweets();
   res.status(200).send(tweets);
 };
